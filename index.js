bd41bfeee95250d38966a38842c363e8b229968e
Singurize <notkendro@gmail.com>
2020-10-01 16:30:07 +0600

    First

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+  console.log('ready')
+})
+
+client.on('message', message => {
+
+  if (message.content === `${prefix}perks`) {
+    message.channel.send('TDA is coming up for open enrollment Perks!! **Members will no threat to transmit HIV to you. We will not send personal Messages to your family. We will not address you but block you before we do. We are Poach Free within the server**. <—- essentially we have pest control. :wink: Additionally We Also Make ||Bots|| out of You, Extra family members cause why not?');
+  }
+
+  if (message.content === `${prefix}rand`) {
+    message.channel.send('you:middle_finger:');
+  }
+  
+  if (message.content === `${prefix}singasong`) {
+    message.channel.send(':musical_note:Margaya Madarch*d LA LA. LA LALLA LA LA LAAA~~:musical_note:');
+  }
+
+})
+
+client.login(process.emv.token);
